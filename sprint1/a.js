ca529a88d2c33cbe38b514aae15ac15a3b5c2e94
Aleksey Denisov <deoz@yandex.ru>
2023-11-16 15:39:38 +0300

    Sprint 1 / A - Значения функции

---
+++ sprint1/a.js
@@ -0,0 +1,48 @@
+/**
+ * Помогите Васе написать код функции, вычисляющей y = ax2 + bx + c.
+ * Напишите программу, которая будет по коэффициентам a, b, c и числу x
+ * выводить значение функции в точке x.
+ **/
+
+const _readline = require("readline");
+
+const _reader = _readline.createInterface({
+  input: process.stdin,
+});
+
+const _inputLines = [];
+let _curLine = 0;
+
+_reader.on("line", (line) => {
+  _inputLines.push(line);
+});
+
+process.stdin.on("end", solve);
+
+function evaluateFunction(a, x, b, c) {
+  return a * (x * x) + b * x + c;
+}
+
+function solve() {
+  const inputNumbers = readArray();
+  const a = inputNumbers[0];
+  const x = inputNumbers[1];
+  const b = inputNumbers[2];
+  const c = inputNumbers[3];
+  process.stdout.write(`${evaluateFunction(a, x, b, c)}`);
+}
+
+function readInt() {
+  const n = Number(_inputLines[_curLine]);
+  _curLine++;
+  return n;
+}
+
+function readArray() {
+  const arr = _inputLines[_curLine]
+    .trim()
+    .split(" ")
+    .map((num) => Number(num));
+  _curLine++;
+  return arr;
+}
